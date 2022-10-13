cb72e9d9f0ed8991d74f526d80f221d02a5c9b27
Douglas de Araujo <douglas.a@academico.ifrn.edu.br>
2022-10-13 10:11:05 -0300

    v.1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-mega-sena';
+  test_array: Array<number> = [];
+
+  public retorna_rand = () => {
+    return Math.floor(Math.random() * 60);
+  };
+
+  public populate_arr = (nros: number) => {
+    for (let i = 0; i <= nros; i++) {
+      this.test_array.push(this.retorna_rand());
+    }
+  };
+  public sort_arr = () => {
+    if (this.test_array.length > 0) {
+      this.test_array.sort((a, b) => a - b);
+    } else {
+      alert('nothing to sort');
+    }
+  };
+  public reset_arr = () => {
+    this.test_array = [];
+  };
 }
